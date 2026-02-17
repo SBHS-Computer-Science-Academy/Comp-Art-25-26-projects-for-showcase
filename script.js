c7baa12b0d84dddfdc270d30458e69179715f9c0
Anthonyp0809 <evapacheco20@yahoo.com>
2026-02-17 14:10:18 -0800

    Added students and project links

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Each name should be in the format "Stu1, Stu2, and Stu3" with an optional URL
 var names = [
-    ["Alessandro", "https://codehs.com/share/id/final-project-MbUEpb/run"]
+    ["Alessandro", "https://codehs.com/share/id/final-project-MbUEpb/run"],
+    ["Ivan", "https://codehs.com/share/id/final-project-891f2h/run"],
+    ["Vonn", "https://codehs.com/share/id/final-project-CGu32v/run"],
+    ["Maddie", "https://codehs.com/share/id/final-project-FICB9H/run"],
+    ["Miles", "https://codehs.com/share/id/final-project-OWkyVF/run"],
+    ["Henry,Mason,Noah", "https://codehs.com/sandbox/kstewart/retro-polo"],
+    ["Mason", "https://codehs.com/sandbox/kstewart/blackjack-p5"],
+    ["Sam,Kaylee","https://codehs.com/sandbox/kstewart/final-pacman"],
+    ["Marsi", "https://codehs.com/share/id/final-project-891f2h/run"],
+    ["Vanessa", "https://codehs.com/share/id/final-project-fy2fVB/run"],
+    ["Nicholas", "https://codehs.com/share/id/final-project-fZETxB/run"],
+    ["Kayleb", "https://codehs.com/share/id/final-project-ETZ9XQ/run"],
+    ["Adam", "https://codehs.com/share/id/final-project-MELUKX/run"],
+    ["Koa", "https://codehs.com/share/id/final-project-UqSd1j/run"]
 ];
 
 var gridWidth = 4;
